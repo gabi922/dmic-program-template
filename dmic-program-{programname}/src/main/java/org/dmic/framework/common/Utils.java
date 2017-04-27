119160f18d75c2442335d5c8aded0cb43d28621d
Gabriel Raducu <gabi.raducu25@gmail.com>
2017-04-27 21:15:06 +0300

    add loadParameters method

--- dmic-program-{programname}/src/main/java/org/dmic/framework/common/Utils.java
+++ dmic-program-{programname}/src/main/java/org/dmic/framework/common/Utils.java
@@ -1,6 +1,8 @@
 package org.dmic.framework.common;
 
 import java.util.Collection;
+import org.apache.commons.io.IOUtils;
+import java.nio.charset.StandardCharsets;
 
 public class Utils {
     public static boolean isNullOrEmpty(String string) {
@@ -10,5 +12,11 @@ public class Utils {
     public static Class<?>[] toArray(Collection<Class<?>> collection) {
         Class<?>[] array = new Class<?>[collection.size()];
         return collection.toArray(array);
+    }
+
+    public static <T> T loadParameters(Class<T> tClass, String location) throws IOException {
+        ConfigParser<T> configParser = new ConfigParser<>(tClass);
+        String parameters = IOUtils.toString(Utils.class.getResourceAsStream(location), StandardCharsets.UTF_8);
+        return configParser.parseString(parameters);
     }
 }
